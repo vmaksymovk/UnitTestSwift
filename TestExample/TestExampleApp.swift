2a4778917058cf73305fdedabd035f79eecc446d
Vladyslav Maksymov <vmaksymovk@gmail.com>
2024-03-05 13:46:09 +0100

    first commit

---
+++ TestExample/TestExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestExampleApp.swift
+//  TestExample
+//
+//  Created by Влад on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TestExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnitTestExample/UnitTestExample.swift
@@ -0,0 +1,36 @@
+
+import XCTest
+@testable import TestExample
+
+public class TestExample {
+    public static func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+}
+
+final class UnitTestExample: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+    func testAddition() throws {
+        // Given
+        let firstNumber = 3
+        let secondNumber = 5
+        
+        // When
+        let result = TestExample.add(firstNumber, secondNumber)
+        
+        // Then
+        XCTAssertEqual(result, 8, "Addition of \(firstNumber) and \(secondNumber) should be 8")
+    }
+
+    
+
+}
